ef9804c29d95772062627da6fdec7d374f776def
Naysa Alex <46577940+naysaalex@users.noreply.github.com>
2023-03-08 15:12:32 -0600

    Create ContentView.swift

---
+++ EdTechPlatform_App/EdTechPlatformApp/Views/ContentView.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct ContentView: View{
+  var module: Module //used to access the module
+var body: some View{
+ScrollView{
+LazyVStack{
+  //ForEach{}
+  //will not let you jump between lessons 
+  
+}//end of lazy
+}//end of scroll
+}//end of var body
+}//end of struct contentview
+
+struct ContentView_Previews: PreviewProvider{
+static var previews: some View{
+ContentView()
+}
+}
